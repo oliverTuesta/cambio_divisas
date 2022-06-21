556b910cc284c14d8307fc4a5f6428b360e71b8d
Oliver Tuesta <oliver.jtuesta@gmail.com>
2022-06-21 08:17:51 -0500

    radix sort added in test.cpp

--- cambio_seguro/test.cpp
+++ cambio_seguro/test.cpp
@@ -1,7 +1,56 @@
 #include "TrieTree.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+// radix sort
+int getMax(vector<int> &v)
+{
+    int max = v[0];
+    for (int i = 1; i < v.size(); i++)
+    {
+        if (v[i] > max)
+        {
+            max = v[i];
+        }
+    }
+    return max;
+}
+
+// count sort
+void countSort(vector<int> &v, int exp)
+{
+    int output[v.size()];
+    int i, count[10] = {0};
+    for (i = 0; i < v.size(); i++)
+    {
+        count[(v[i] / exp) % 10]++;
+    }
+    for (i = 1; i < 10; i++)
+    {
+        count[i] += count[i - 1];
+    }
+    for (i = v.size() - 1; i >= 0; i--)
+    {
+        output[count[(v[i] / exp) % 10] - 1] = v[i];
+        count[(v[i] / exp) % 10]--;
+    }
+    for (i = 0; i < v.size(); i++)
+    {
+        v[i] = output[i];
+    }
+}
+
+// radix sort
+void radixSort(vector<int> &v)
+{
+    int m = getMax(v);
+    for (int exp = 1; m / exp > 0; exp *= 10)
+    {
+        countSort(v, exp);
+    }
+}
 
 // C++ implementation of Trie data structure
 int main()
@@ -54,6 +103,21 @@ int main()
     }
 
     cout << head->search("hell"); // print 0
+
+    cout << "\n\nRadix sort: \n";
+    // testing radix sort
+    vector<int> v = {11, 2, 53, 7, 8, 9, 1, 5, 6, 10};
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    radixSort(v);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
